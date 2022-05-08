2c6216d8470eb485fd88eac32022de030c8a484d
lebrancconvas <lebrancconvas@gmail.com>
2022-05-08 23:46:12 +0700

    ✨ List Monster Card Name to the Website

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,17 @@
 import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 import router from './controllers/router';
 
 const app: Application = express();
 const PORT = process.env.PORT || 3002;
 
+const corsOptions = {
+	origin: 'http://localhost:3000',
+	credentials: true
+}
+
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use(router);
 
 app.listen(PORT, () => {
--- web/src/components/CardSection.tsx
+++ web/src/components/CardSection.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios, {AxiosResponse} from 'axios';
 
 const CardSection: React.FC = () => {
+	const [carddata, setCarddata] = useState([]);
+
+	useEffect(() => {
+		axios.get('http://localhost:3002/api/v1/cards/monster-cards', {headers: {'Content-Type': 'application/'}})
+			.then((response: AxiosResponse) => response.data)
+			.then(data => setCarddata(data))
+			.catch(err => console.error(err))
+	})
+
 	return(
 		<div>
 			<h1 className="text-white text-2xl">CardSection</h1>
+			{carddata.map((card: any, index: number) => {
+				return(
+					<div className="text-white" key={index}>
+						{card.name}
+					</div>
+				)
+			})}
 		</div>
 	);
 };
